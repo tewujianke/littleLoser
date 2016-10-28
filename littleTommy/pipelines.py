0e2ea0e17f979ee017bffe833422245b740bd688
Guanduo <liguanduo@gmail.com>
2016-10-28 13:46:10 -0700

    enable basic pipeline architecture

--- littleTommy/pipelines.py
+++ littleTommy/pipelines.py
@@ -5,7 +5,24 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+#import scrapy.exceptions.DropItem as DropException
+#import scrapy.exceptions.DropItem
+from scrapy.exceptions import DropItem
+from scrapy.exporters import XmlItemExporter
+import os.path
 
 class LittletommyPipeline(object):
     def process_item(self, item, spider):
+
+        if(not os.path.exists('store.log')):
+            db = open('store.log','w')
+            db.write("#supreme log")
+        else:
+            db = open('store.log','a')
+        
+        if not item['valid'] or item['valid'] == '0':
+            raise DropException('item not valid')
+        feed = XmlItemExporter(db)
+
+        db.close()
         return item
--- littleTommy/settings.py
+++ littleTommy/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = 'littleTommy.spiders'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
-
+FEED_FORMAT = 'xml'
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'littleTommy.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'littleTommy.pipelines.LittletommyPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
