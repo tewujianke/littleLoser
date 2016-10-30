e88393bd5ab2c358b984c9b40f3c1dcf80d0f71a
Guanduo <liguanduo@gmail.com>
2016-10-30 01:11:25 -0700

    add a script to clean .pyc .xml .log

---
+++ littleTommy/clean.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""
+Author: Guanduo
+chmod +x clean.py
+./clean.py will work base shebang above
+default is to clean all .pyc files recursively form current dir to all sub
+-all: clean all .pyc, .xml, .log
+-xml: clean .pyc, .xml
+-log: clean .pyc, .log
+"""
+from __future__ import print_function
+from sys import argv
+import glob
+from os import remove
+from os.path import isdir
+#add more search dir here
+dirs = []
+files = glob.glob('*')
+for f in files:
+    if isdir(f):
+        dirs.append('./'+str(f)+'/')
+#don't forgot current dir
+dirs.append('./')
+
+verbose = 0
+for arg in argv:
+    if arg == "-xml":
+        clean_xml = 1
+    else:
+        clean_xml = 0
+    if arg == "-log":
+        clean_log = 1
+    else:
+        clean_log = 0
+    if arg == '-v':
+        verbose = 1
+    if arg == "-all":
+        clean_xml = 1
+        clean_log = 1
+
+        #default clean log only
+def clc_dir(path,xml=0,log=0):
+    global verbose
+    if verbose:
+        print("found path="+path)
+    files = glob.glob('%s*.pyc'%path)
+    for i in files:
+        print("del %s"%(i))
+        remove("%s"%(i))
+    if xml:
+        files = glob.glob('%s*.xml'%path)
+        for i in files:
+            print("del %s"%(i))
+            remove("%s"%(i))
+    if log:
+        files = glob.glob('%s*.log'%path)
+        for i in files:
+            print("del %s"%(i))
+            remove("%s"%(i))
+          
+for di in dirs:
+    
+    clc_dir('%s'%di,clean_xml,clean_log)    
